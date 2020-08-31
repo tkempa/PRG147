b24bfb0a01b3d79c465a45b2c26b75578d0ddcbc
tkempa <42559394+tkempa@users.noreply.github.com>
2020-08-31 16:00:00 -0500

    Add files via upload

---
+++ 03/test.js
@@ -0,0 +1,40 @@
+var a = 5;
+var b = 4;
+var sum = a + b;
+
+var a, b, sum;
+a = 5;
+b = 4;
+sum = a + b;
+
+var a = 5, b = 4, sum = a + b;
+
+var negInteger = -3;
+var escQuote = "Quotes can also be \"escaped\".";
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+
+console.log(typeof negInteger);
+console.log(typeof escQuote);
+console.log(typeof theSunIsWarm);
+console.log(typeof emptyInside);
+console.log(typeof justAnotherVariable);
+
+var a = 1;
+console.log(a);
+console.log(++a);
+console.log(a++);
+console.log(a);
+
+var a= 4;
+var b = "ducks";
+console.log(a+b)
+
+var a = 5;
+var b = 5;
+var theNumbersMatch;
+
+if (a == b) theNumbersMatch = true;
+else theNumbersMatch = false;
+console.log(theNumbersMatch);

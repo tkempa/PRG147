041d753f24351d2327a8a87394e06378d8a1ad8e
tkempa <42559394+tkempa@users.noreply.github.com>
2020-10-07 14:46:20 -0500

    Add files via upload

---
+++ 15/src/main.js
@@ -0,0 +1,31 @@
+//set up game
+let clickers = 4;
+let startTime = Date.now();
+document.querySelector("#score").innerHTML = clickers + " ricks remain";
+for (let i = 0; i < clickers; i++) addClicker();
+
+//add a clicker
+function addClicker() {
+    let img = new Image();
+    img.src = "res/images/rick.png";
+    img.style.left = Math.random() * 500 + "px";
+    img.style.top = Math.random() * 300 + "px";
+    img.addEventListener("click", removeClicker);
+    document.querySelector("#board").appendChild(img);
+}
+
+//remove a clicker and update remaining ricks
+function removeClicker(e) {
+    e.target.parentNode.removeChild(e.target);
+    clickers--;
+    document.querySelector("#score").innerHTML = clickers + " ricks remain";
+    checkGameOver();
+}
+
+//check for a game over and show the score
+function checkGameOver() {
+    if (clickers === 0) {
+        const taken = Math.round((Date.now() - startTime) / 1000);
+        alert("De-rick-ed in " + taken + " seconds!");
+    }
+}
